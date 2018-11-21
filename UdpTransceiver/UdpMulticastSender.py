f4337de6e97755795fdf6bcd9da61d555e4eb028
Wouter Dankers <wouter.dankers@volvo.com>
2018-11-21 14:07:35 +0100

    spelling mistake corrected

--- UdpTransceiver/UdpMulticastSender.py
+++ UdpTransceiver/UdpMulticastSender.py
@@ -1,17 +1,22 @@
 import socket
+import time
 
 multicast_group = '224.1.1.1'
 multicast_port = 5007
 content = "Hello from the other side! "
+
 # regarding socket.IP_MULTICAST_TTL
 # ---------------------------------
 # for all packets sent, after two hops on the network the packet will not
 # be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
+
 MULTICAST_TTL = 2
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 
-for i in range(10):
+for i in range(1000):
     message = content + str(i)
     sock.sendto(message, (multicast_group, multicast_port))
+    print "Message: " + message
+    time.sleep(1)

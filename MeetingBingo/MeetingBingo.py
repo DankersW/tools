59271f482bf6900f5d1a0e909d58bb905ac054e8
Wouter Dankers <wouter.dankers@volvo.com>
2019-04-10 07:51:00 +0200

    meeting bingo added

--- MeetingBingo/MeetingBingo.py
+++ MeetingBingo/MeetingBingo.py
@@ -1,9 +1,17 @@
 from random import shuffle
-from prettytable import PrettyTable
+import xlsxwriter
 
 
 class MeetingBingo:
     buzz_words = []
+
+    MAX_ROWS = 3
+    MAX_BINGO_ITEMS = 9
+
+    title = 'Meeting room Bingo'
+    description = 'To win connect 3 buzz words with a straight line.\n' \
+                  'If you shout "BINGO" out loud during the meeting, Tommy Svensson buys you a pizza and a beer\n' \
+                  'Good Luck!'
 
     def __init__(self):
         with open('buzz_words.txt', 'r') as f:
@@ -13,12 +21,46 @@ class MeetingBingo:
         shuffle(self.buzz_words)
 
     def __call__(self, *args, **kwargs):
-        print self.buzz_words
-        t = PrettyTable(['Name', 'Age'])
-        t.add_row(['Alice', 24])
-        t.add_row(['Bob', 19])
-        print t
+        workbook = xlsxwriter.Workbook('meeting_bingo.xlsx')
+        worksheet = workbook.add_worksheet()
 
+        title_format = workbook.add_format({
+            'bold': True,
+            'border': True,
+            'align': 'center',
+            'valign': 'vcenter',
+            'fg_color': 'yellow'
+        })
+
+        description_format = workbook.add_format({
+            'bold': True,
+            'border': True,
+            'align': 'left',
+            'valign': 'vcenter',
+            'fg_color': 'yellow',
+            'text_wrap': True
+        })
+
+        buzz_words_format = workbook.add_format({
+            'bold': True,
+            'border': True,
+            'align': 'center',
+            'valign': 'vcenter'
+        })
+
+        worksheet.set_column('A:C', 25)
+        for i in range(self.MAX_ROWS+3):
+            worksheet.set_row(i, 40)
+
+        worksheet.merge_range('A1:C1', self.title, title_format)
+        worksheet.merge_range('A2:C3', self.description, description_format)
+
+        for i in range(self.MAX_BINGO_ITEMS):
+            col = i / self.MAX_ROWS
+            row = i - (self.MAX_ROWS * col)
+            worksheet.write(col+3, row, self.buzz_words[i], buzz_words_format )
+
+        workbook.close()
 
 
 if __name__ == '__main__':

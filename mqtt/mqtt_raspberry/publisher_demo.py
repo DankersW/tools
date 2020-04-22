5720b2198c4278cd53bdb1bf9f534a49a959ce87
DankersW <wouter.dankers@skynet.be>
2020-04-22 14:22:13 +0200

    topic and data are arguments now

--- mqtt/mqtt_raspberry/publisher_demo.py
+++ mqtt/mqtt_raspberry/publisher_demo.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
+# usage python3 publisher_demo.py [topic] [data]
 
 import paho.mqtt.client as mqtt
+import sys
 
-MQTT_BROKER_ADDRESS = "10.42.0.25"
+MQTT_BROKER_ADDRESS = "192.168.1.125"
 MQTT_PORT = 1883
 MQTT_STAYALIVE = 60
 
+topic = sys.argv[1]
+data = sys.argv[2]
+
+print("topic: " + str(topic) + "\t data: " + str(data))
+
 client = mqtt.Client()
 client.connect(MQTT_BROKER_ADDRESS, MQTT_PORT, MQTT_STAYALIVE)
-client.publish("topic/test", "Hello world!")
+client.publish(topic, data)
 client.disconnect()

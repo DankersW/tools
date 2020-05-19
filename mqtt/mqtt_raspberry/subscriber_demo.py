0e51c76caf326752feaeeae979a0f858ba9da7eb
DankersW <wouter.dankers@skynet.be>
2020-05-19 18:44:42 +0200

    made the test subscriber listen to all messages

--- mqtt/mqtt_raspberry/subscriber_demo.py
+++ mqtt/mqtt_raspberry/subscriber_demo.py
@@ -2,21 +2,22 @@
 
 import paho.mqtt.client as mqtt
 import datetime
+import sys
 
 MQTT_BROKER_ADDRESS = "10.42.0.25"
 MQTT_PORT = 1883
 MQTT_STAYALIVE = 60
 
+TOPICS = []
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+ str(rc))
-    client.subscribe("topic/test")
-
+    print("Connected with result code " + str(rc))
+    client.subscribe("#")
 
 def on_message(client, userdata, msg):
     time = datetime.datetime.now()
     payload = msg.payload.decode('utf-8')
-    print("{} | Received message: {}".format(time, payload))
+    print("{} | Received message: \"{}\" on Topic: \"{}\"".format(time, payload, msg.topic))
 
 
 client = mqtt.Client()
